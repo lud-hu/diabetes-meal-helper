da1890852fbfd346ee76e9ebb4fd117aa5555217
Ludwig H. | polarstern <ludwig.h@polarstern-energie.de>
2024-03-15 23:43:42 +0100

    hello world

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+    CollectionReference,
+    DocumentData,
+    Firestore,
+    collection,
+    connectFirestoreEmulator,
+    getFirestore,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,5 +20,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-export default app;
+const emulatorHost = "127.0.0.1";
+if (window.location.hostname === "localhost") {
+    console.log("Detected local usage, point to emulators now.");
+    connectFirestoreEmulator(db, emulatorHost, 8080);
+}
+
+const createCollection = <T = DocumentData>(firestore: Firestore, collectionName: string) => {
+    return collection(firestore, collectionName) as CollectionReference<T>;
+};
+
+export { app, db, createCollection };
